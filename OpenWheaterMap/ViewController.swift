9c2c64f0837813440456efa23b8caf111fc17d80
Thiago Santos <t.vaz.santos@gmail.com>
2019-04-27 08:58:12 -0300

    Initial Commit

---
+++ OpenWheaterMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenWheaterMap
+//
+//  Created by THIAGO on 27/04/19.
+//  Copyright © 2019 THIAGO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
